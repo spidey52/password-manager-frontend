b8ff16d5925c51a8da04211425f7a8fffc4e55ff
spidey52 <satyamkumar5254@gmail.com>
2020-07-23 15:09:16 +0530

    Basic functionality is done. except of password update and delete i am very excited to check out these things.

--- script.js
+++ script.js
@@ -1,154 +1,139 @@
 const baseUrl = "http://localhost:3000"
-const login_button = document.querySelector('#login_button')
 
-
-if (isAuthenticated()) {
-    renderPasswords()
-}
-else {
-    renderLoginPage()
-}
-
-optionBtn.forEach(item => {
-    item.addEventListener('click', e => {
-        optionMenu.forEach(item => {
-            hideElement(item)
-        })
-        showElement(e.target.parentElement.parentElement.children[1])
-    })
-})
-
-function showElement(el) {
-    el.classList.remove('hidden')
-}
-function hideElement(el) {
-    el.classList.add('hidden')
-}
-
-window.addEventListener('click', function (event) {
-    if (!event.target.matches('.dot')) {
-        optionMenu.forEach(item => {
-            hideElement(item)
-        })
-        console.log('hidden')
+class User {
+    static getAuthToken() {
+        return localStorage.getItem('login_token')
     }
-})
 
-function isAuthenticated() {
-    return localStorage.getItem('login_token')
-}
+    static isAuthenticated() {
+        if (User.getAuthToken() === 'undefined' || User.getAuthToken() === null) {
+            return false
+        }
+        return true
+    }
 
-function submitForm() {
-    document.querySelector('form')
-        .addEventListener('submit', async (e) => {
-            e.preventDefault()
-            const email = e.srcElement[0].value
-            const password = e.srcElement[1].value
-            try {
-                const res = await login(email, password)
-                if (res.status !== 200) {
-                    const data = await res.json()
-                    const p = document.createElement('p')
-                    p.innerHTML = data.error
-                    const err_input = document.getElementById('input_error')
-                    err_input.innerHTML = ''
-                    err_input.appendChild(p)
-                    return ''
-                }
-                const data = await res.json()
+    static async signup(username, firstName, lastName, email, password) {
+        const data = await createUserOrError(username, firstName, lastName, email, password)
 
-                console.log('data print')
-                console.log(data)
+        console.log(data)
 
-                localStorage.setItem('login_token', data.token)
-                getAllPasswords(data.token)
-                renderPasswords()
+        if (!data.user) {
+            console.log(data.error)
+            return { error: data.error }
+        }
+        else {
+            localStorage.setItem('login_token', data.user.token)
+            return { error: null, user: data.user.user }
+        }
+    }
+
+    static async login(email, password) {
+        const data = await getUserOrError(email, password)
+
+        if (!data.user) {
+            console.log(data.error)
+            return { error: data.error }
+        }
+        else {
+            localStorage.setItem('login_token', data.user.token)
+            return { error: null, user: data.user.user }
+        }
+    }
+
+    static async logout() {
+        if (this.isAuthenticated()) {
+            const token = this.getAuthToken()
+            const status = await logOutUserOrError(token)
+            if (!status.done) {
+                console.log(status.error)
+                return status
             }
-            catch (err) {
-                console.log('error printing')
-                console.log(err)
-            }
-        })
-}
-
-document.getElementById('logout_button')
-    .addEventListener('click', async (e) => {
-        if (!isAuthenticated()) {
-            return alert('you must login first')
+            console.log(status.done)
+            return status
         }
-        try {
-            const res = await logout(isAuthenticated())
-            const data = await res.json()
+    }
+
+    static async getPasswordList() {
+        const data = await getAllPasswords(this.getAuthToken())
+        if (!data.password) {
             console.log(data)
-            localStorage.removeItem('login_token')
-            renderLoginPage()
-        } catch (error) {
-            return console.log(error)
+            return undefined
         }
-    })
-
-document.getElementById('logoutAll_button')
-    .addEventListener('click', async (e) => {
-        if (!isAuthenticated()) {
-            return alert('you must login first')
-        }
-        try {
-            const res = await logoutAll(isAuthenticated())
-            const data = await res.json()
-            localStorage.removeItem('login_token')
-            console.log(data)
-            renderLoginPage()
-        } catch (error) {
-            return console.log(error)
-        }
-    })
-
-
-function login(email, password) {
-    return fetch(`${baseUrl}/users/login`, {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ email, password })
-    })
-}
-
-function logout(token) {
-    return fetch(`${baseUrl}/users/logout`, {
-        method: "POST",
-        headers: { "Content-type": "application/json", "Authorization": token },
-    })
-}
-
-function logoutAll(token) {
-    return fetch(`${baseUrl}/users/logoutAll`, {
-        method: "POST",
-        headers: { "Content-type": "application/json", "Authorization": token }
-    })
-}
-
-
-async function getAllPasswords(token) {
-    try {
-        const res = await fetch(`${baseUrl}/passwds`, {
-            method: "GET",
-            headers: { "Content-type": "application/json", "Authorization": token }
-        })
-        return await res.json()
-    } catch (err) {
-        return err
+        return await data.password
     }
 }
 
-async function renderPasswords() {
-    const data = await getAllPasswords(isAuthenticated())
+class UI {
+    static showLoginForm() {
+        const formContainer = document.createElement('div')
+        formContainer.innerHTML = `
+         <form id="login_form">
+        <div class="form-div">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email">
+            <span id="input_error"> </span> 
+        </div>
+        <div class="form-div">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div class="form-div">
+            <button class="login">login</button>
+        </div>
+    </form>
+        `
+        const mainContainer = document.querySelector('.container')
+        mainContainer.innerHTML = ''
+        mainContainer.appendChild(formContainer)
+    }
 
-    const container = document.querySelector('.container')
-    container.innerHTML = ''
+    static showSignupForm() {
+        const formContainer = document.createElement('div')
+        formContainer.innerHTML = `
+        <form id="signup_form">
+        <div class="form-div">
+            <label for="username">username</label>
+            <input type="text" name="" id="username">
+        </div>
+        <div class="form-div">
+            <label for="firstname">first name</label>
+            <input type="text" name="" id="firstname">
+        </div>
+        <div class="form-div">
+            <label for="lastname">lastname</label>
+            <input type="text" name="", id="lastname">
+        </div>
+        <div class="form-div">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div class="form-div">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div class="form-div">
+            <button class="login">login</button>
+        </div>
+    </form>
+        `
 
-    data.forEach(passwd => {
-        const password = document.createElement('div')
-        password.classList.add('password')
-        password.innerHTML = `
+        const mainContainer = document.querySelector('.container')
+        mainContainer.innerHTML = ''
+        mainContainer.appendChild(formContainer)
+    }
+
+    static async showPasswordList() {
+        if (!User.isAuthenticated()) {
+            return false
+        }
+        const passwds = await User.getPasswordList()
+        const container = document.querySelector('.container')
+        container.innerHTML = ''
+
+        passwds.forEach(passwd => {
+            const password = document.createElement('div')
+            password.classList.add('password')
+            password.innerHTML = `
             <div class="option">
                 <div class="dot">.</div>
                 <div class="dot">.</div>
@@ -168,39 +153,268 @@ async function renderPasswords() {
                 <button class="clipboard-button">clipboard</button>
             </div>
         `
-        container.appendChild(password)
-    })
-    init()
-}
-
-function renderLoginPage() {
-    document.querySelector('.container')
-        .innerHTML = `
-    <form>
-        <div class="form-div">
-            <label for="email">Email</label>
-            <input type="text" name="email" id="email">
-            <span id="input_error" > </span> 
-        </div>
-        <div class="form-div">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div class="form-div">
-            <button class="login">login</button>
-        </div>
-    </form>
-        `
-
-    submitForm()
-}
-
-function init() {
-    document.querySelectorAll('.clipboard-button').forEach(item => {
-        item.addEventListener('click', e => {
-            e.target.parentElement.children[0].select()
-            document.execCommand('copy')
+            container.appendChild(password)
         })
-    })
+    }
+
+    static showErrorMsg(msg, tp) {
+        const errDiv = document.createElement('div')
+        errDiv.className = `err err-${tp}`
+        errDiv.textContent = msg
+        const err_container = document.querySelector('.err-container')
+        err_container.innerHTML = ''
+        err_container.appendChild(errDiv)
+
+        setTimeout(() => {
+            errDiv.remove()
+        }, 3000)
+    }
 }
 
+
+class Password {
+
+    static async addPassword(name, password) {
+        const res = await fetch(`${baseUrl}/passwds`, {
+            method: "POST",
+            headers: { "Content-type": "application/json", Authorization: User.getAuthToken() },
+            body: JSON.stringify({ name, password })
+        })
+
+        if (res.status !== 201) {
+            return { error: 'something went wrong' }
+        }
+        else {
+            main()
+        }
+    }
+
+    static updatePassword() {
+
+    }
+
+    static removePassword() {
+
+    }
+}
+
+async function createUserOrError(username, firstName, lastName, email, password) {
+    try {
+        const res = await fetch(`${baseUrl}/users`, {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ username, firstName, lastName, email, password })
+        })
+        if (res.status !== 201) {
+            const data = await res.json()
+            return { user: null, error: data.error }
+        }
+
+        const data = await res.json()
+        return { user: data }
+    }
+    catch (error) {
+        return { user: null, error: 'server error' }
+    }
+}
+
+async function getUserOrError(email, password) {
+    try {
+        const res = await fetch(`${baseUrl}/users/login`, {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
+        if (res.status !== 200) {
+            const data = await res.json()
+            return { user: null, error: data.error }
+        }
+        const data = await res.json()
+        return { user: data }
+    }
+    catch (error) {
+        return { user: null, error: "server error" }
+    }
+}
+
+async function logOutUserOrError(token) {
+    try {
+        const res = await fetch(`${baseUrl}/users/logout`, {
+            method: "POST",
+            headers: { "Content-type": "application/json", "Authorization": token },
+        })
+
+        if (res.status !== 200) {
+            const data = await res.json()
+            return { done: false, error: data }
+        }
+        localStorage.removeItem('login_token')
+        return { done: true }
+    } catch (error) {
+        return { done: false, error }
+    }
+}
+
+async function getAllPasswords(token) {
+    try {
+        const res = await fetch(`${baseUrl}/passwds`, {
+            method: "GET",
+            headers: { "Content-type": "application/json", "Authorization": token }
+        })
+        if (res.status !== 200) {
+            const data = await res.json()
+            return { password: null, error: data }
+        }
+        const data = await res.json()
+        return { password: data }
+    } catch (err) {
+        return { password: null, error: err }
+    }
+}
+
+function main() {
+    if (User.isAuthenticated()) {
+
+        const addContainer = document.querySelector('.add-container')
+        addContainer.classList.remove('hidden')
+
+        const addForm = document.querySelector('#add_form')
+        addForm.addEventListener('submit', async (e) => {
+            e.preventDefault()
+
+            const name = e.target[0].value
+            const password = e.target[1].value
+
+            const status = await Password.addPassword(name, password)
+            if (status) {
+                UI.showErrorMsg(status.error, 'danger')
+            }
+            UI.showErrorMsg('added succesfully', 'success')
+            e.target[0].value = ''
+            e.target[1].value = ''
+            console.log(status)
+
+        })
+
+        UI.showPasswordList()
+
+        const login_button = document.querySelector('#login_button')
+        const signup_button = document.querySelector('#signup_button')
+
+        if (login_button) {
+            login_button.remove()
+        }
+        if (signup_button) {
+            signup_button.remove()
+        }
+
+        const userNameButton = document.createElement('span')
+        userNameButton.classList.add('nav-button')
+        userNameButton.id = "profile_button"
+        userNameButton.textContent = 'profile'
+
+        const logOutBtn = document.createElement('span')
+        logOutBtn.classList.add('nav-button')
+        logOutBtn.id = "logout_button"
+        logOutBtn.textContent = 'logout'
+
+
+        document.querySelector('.nav-links').appendChild(userNameButton)
+        document.querySelector('.nav-links').appendChild(logOutBtn)
+
+        logOutBtn.addEventListener('click', async (e) => {
+            const status = await User.logout()
+            if (status.done) {
+                return main()
+            }
+        })
+        console.log('main called')
+    }
+
+    else {
+        document.querySelector('.add-container').classList.add('hidden')
+        UI.showLoginForm()
+        const login_button = document.createElement('span')
+        login_button.classList.add('nav-button')
+        login_button.id = "login_button"
+        login_button.innerHTML = 'login'
+
+        const signup_button = document.createElement('span')
+        signup_button.classList.add('nav-button')
+        signup_button.id = 'signup_button'
+        signup_button.innerHTML = 'signup'
+
+        const profile_button = document.querySelector('#profile_button')
+        const logout_button = document.querySelector('#logout_button')
+
+        let isUser = false
+        signup_button.addEventListener('click', (e) => {
+            UI.showSignupForm()
+            document.querySelector('#username').addEventListener('input', async (e) => {
+                if (e.target.value !== '') {
+                    const res = await fetch(`${baseUrl}/users/checkusername/${e.target.value}`, { method: 'GET', headers: { "Content-type": "application/json" } })
+                    const data = await res.json()
+                    if (data.msg === false) {
+                        isUser = true
+                        UI.showErrorMsg('usename already exists', 'danger')
+                    } else {
+                        isUser = false
+                    }
+                }
+            })
+
+            const signup_form = document.querySelector('#signup_form')
+            signup_form.addEventListener("submit", async (e) => {
+                e.preventDefault()
+                const username = e.target[0].value
+                const firstname = e.target[1].value
+                const lastname = e.target[2].value
+                const email = e.target[3].value
+                const password = e.target[4].value
+
+                const status = await User.signup(username, firstname, lastname, email, password)
+
+                if (status.error) {
+                    UI.showErrorMsg(status.error, 'danger')
+                    return console.log(status.error)
+                }
+                main()
+            })
+        })
+
+        login_button.addEventListener('click', (e) => {
+            UI.showLoginForm()
+            main()
+        })
+
+        if (profile_button) {
+            profile_button.remove()
+        }
+        if (logout_button) {
+            logout_button.remove()
+        }
+        const nav_links = document.querySelector('.nav-links')
+        nav_links.innerHTML = ''
+        nav_links.appendChild(login_button)
+        nav_links.appendChild(signup_button)
+
+        const login_form = document.querySelector('#login_form')
+        login_form.addEventListener("submit", async (e) => {
+            e.preventDefault()
+            const email = e.target[0].value
+            const password = e.target[1].value
+
+            const status = await User.login(email, password)
+            if (status.error) {
+                UI.showErrorMsg(status.error, 'danger')
+                return console.log(status.error)
+            }
+            console.log('this one ')
+            main()
+        })
+
+
+    }
+}
+
+main()
